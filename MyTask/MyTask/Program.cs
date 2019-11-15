0ea3b6bddb64c6f611702d1c3b71ee4bb9b7bd36
AlexPankovSaratov <56839541+AlexPankovSaratov@users.noreply.github.com>
2019-11-15 20:23:25 +0400

    0.1 and 0.2 Tasks

--- MyTask/MyTask/Program.cs
+++ MyTask/MyTask/Program.cs
@@ -10,6 +10,21 @@ namespace MyTask
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Sequence(8));
+            Console.ReadKey();
+        }
+        static String Sequence(int N)
+        {
+            String result = N.ToString();
+            if(N > 1)
+            {
+                result = Sequence(N - 1) + ","+  result;
+            }
+            else
+            {
+                result = 1.ToString();
+            }
+            return result;
         }
     }
 }
