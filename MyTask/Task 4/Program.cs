752336c650a8f39e9d5f3cf56ce73fcde1b5bfd3
PankovAlex <pankov92@mail.ru>
2019-12-08 18:34:55 +0400

    Task 4 in the Process

--- MyTask/Task 4/Program.cs
+++ MyTask/Task 4/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Task_4
 {
@@ -12,51 +14,131 @@ namespace Task_4
         {
             //CustomSort
 
-            string [] Array = new string[] { "Привет как твои дела?", "Как дела?", "А Как дела?", "Б Как дела?", "Дела?" };
-            CustomSort(Array, moreA);
-            //CustomSort(Array, moreAInAlphabetOrder);
-            foreach (var item in Array)
+            //string [] Array = new string[] { "Привет как твои дела?", "Как дела?", "АБ Как дела?", "АА Как дела?", "Дела?" };
+            //CustomSort(Array, moreA);
+
+            //foreach (var item in Array)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // NumberArraySum
+            //int[] intArr = new int[] { 1, 2, 3,-5 };
+            //Console.WriteLine("sum = " + intArr.NumberArraySum());
+
+            // TO INT OR NOT TO INT
+            //Console.WriteLine("3425342".ToPositiveInt());
+
+            //I SEEK YOU (Ищу все положительные элементы в массиве)
+
+            TimePrint();
+        }
+        static int []GetRandomArr(int count = 10000, int minValue = -100, int maxValue = 100)
+        {
+            int[] Arr = new int[count];
+            Random rand = new Random();
+            for (int i = 0; i < count; i++) Arr[i] = rand.Next(minValue,maxValue);
+            return Arr;
+        }
+        static long TimeMEtod()
+        {
+            int[] ArrInt = GetRandomArr().ToArray();
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            ArrInt = AllPositiveElements(ArrInt, ElementPositive);
+            time.Stop();
+            return time.ElapsedMilliseconds;
+        }
+        static void TimePrint()
+        {
+            long resulttime = 0;
+            long time = 0;
+            for (int k = 0; k < 10; k++)
             {
-                Console.WriteLine(item);
+                time = TimeMEtod();
+                Console.WriteLine("Время вызова: " + time);
+                resulttime += time;
             }
-
-            //
-
+            Console.WriteLine("время работы AllPositiveElements: " + resulttime + " мс");
             Console.ReadLine();
         }
-        static T[] CustomSort<T>(T[] Array, Func<T, T, bool> SortFunction)
+        static void CustomSort<T>(T[] Array, Func<T, T, bool> ElementComparison)
         {
+            if (ElementComparison == null) return;
             T tempValue;
             for (int i = 0; i < Array.Count() - 1; i++)
             {
                 for (int j = i + 1; j <= Array.Count() - 1; j++)
-                    if (SortFunction.Invoke(Array[j], Array[i])) //Array[j] < Array[i]
+                    if (ElementComparison.Invoke(Array[j], Array[i])) //Array[j] < Array[i]
                     {
                         tempValue = Array[i];
                         Array[i] = Array[j];
                         Array[j] = tempValue;
                     }
             }
-            return Array;
         }
         static bool moreA(string A, string B)
         {
+            if(A.Length == B.Length)
+            {
+                char[] CharArrayA = A.ToCharArray();
+                char[] CharArrayB = B.ToCharArray();
+                foreach (char itemA in CharArrayA)
+                {
+                    foreach (char itemB in CharArrayB)
+                    {
+                        if (itemA != itemB) return itemA < itemB;
+                    }
+                }
+                return false;
+            }
             if (A.Length < B.Length) return true;
             return false;
         }
-        static bool moreAInAlphabetOrder(string A, string B)
+        static void CustomSortInAnotherThread<T>(T [] Array, Func<T, T, bool> ElementComparison)
         {
-            bool result = false;
-            char[] CharArrayA = A.ToCharArray();
-            char[] CharArrayB = B.ToCharArray();
-            foreach (char itemA in CharArrayA)
+            //Thread myThread = new Thread();
+        }
+        public static int[] AllPositiveElements(int[] Array)
+        {
+            IList<int> ValidList = new List<int>();
+            foreach (int item in Array)
             {
-                foreach (char itemB in CharArrayB)
-                {
-                    if (itemA != itemB) return itemA < itemB;
-                }
+                if (item >= 0) ValidList.Add(item);
             }
-            return false;
+            return ValidList.ToArray();
+        }
+        public static T[] AllPositiveElements<T>(T[] Array, Func<T, bool> ElementValidation)
+        {
+            if (ElementValidation == null) return Array;
+            IList < T > ValidList = new List<T>();
+            foreach (T item in Array)
+            {
+                if (ElementValidation(item)) ValidList.Add(item);
+            }
+            return ValidList.ToArray();
+        }
+        static bool ElementPositive(int item) => item >= 0;
+    }
+    public static class Extension
+    {
+        public static int NumberArraySum(this int[] Array)
+        {
+            int sum = 0;
+            foreach (int item in Array)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+        public static bool ToPositiveInt(this string str)
+        {
+            if (str.Length > Int32.MaxValue) return false;
+            foreach (Char item in str.ToCharArray())
+            {
+                if(!Char.IsDigit(item)) return false;
+            }
+            return true;
         }
     }
 }
